d7a4326da5ec8dacf06b63e4023ed6fb19d25745
parkjihyo <wlgy5678@gmail.com>
2026-02-26 14:58:50 +0900

    first commit

---
+++ springBoot-0226/src/main/java/com/busanit501/springboot0226/SpringBoot0226Application.java
@@ -0,0 +1,13 @@
+package com.busanit501.springboot0226;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot0226Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot0226Application.class, args);
+    }
+
+}
